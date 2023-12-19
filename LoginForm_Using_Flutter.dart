a3ae781cdbf3968666fd3afa12cc06765325cbb9
ambuj152 <ambuj152@gmail.com>
2023-12-19 14:48:11 +0530

    feting data from form

--- LoginForm_Using_Flutter.dart
+++ LoginForm_Using_Flutter.dart
@@ -18,66 +18,79 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  var emailText=TextEditingController();
+  var passText=TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('MyFirstFlutterCode'),
-        ),
-        body: Center(
-            child: Container(
-                width: 300,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Login Form Using Flutter"),
-                      Container(height: 20),
-                      TextField(
-                          decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(11),
-                            borderSide:
-                                BorderSide(color: Colors.black, width: 2)),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(11),
-                            borderSide:
-                                BorderSide(color: Colors.blueAccent, width: 2)),
-                        labelText: 'Username',
-                      )),
-                      Container(
-                        height: 11,
-                      ),
-                      TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(11),
-                                  borderSide: BorderSide(
-                                      color: Colors.black, width: 2)),
-                              focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(11),
-                                  borderSide: BorderSide(
-                                      color: Colors.blueAccent, width: 2)),
+      appBar: AppBar(
+        title: Text('MyFirstFlutterCode'),
+      ),
+      body: Center(
+        child:Container(width:300,
+                        child:Column(
+                          mainAxisAlignment:MainAxisAlignment.center,
+                          children:[Text("Login Form Using Flutter"),Container(height:20),
+                            TextField(
+                            controller:emailText,  
+                            decoration:InputDecoration(
+                              border:OutlineInputBorder(
+                              borderRadius:BorderRadius.circular(11),
+                              borderSide:BorderSide(
+                              color: Colors.black,width:2) 
+                              ),
+                              focusedBorder:OutlineInputBorder(
+                              borderRadius:BorderRadius.circular(11),
+                              borderSide:BorderSide(
+                              color: Colors.blueAccent,width:2) 
+                              ),
+                              labelText: 'Username',
+                             
+                            ))
+                          
+                        ,Container(height:11,),
+                          
+                            TextField(
+                              controller:passText,
+                              obscureText: true,
+                              decoration:InputDecoration(
+                              
+                              border:OutlineInputBorder(
+                              borderRadius:BorderRadius.circular(11),
+                              borderSide:BorderSide(
+                              color: Colors.black,width:2) 
+                              ),
+                              focusedBorder:OutlineInputBorder(
+                                
+                              borderRadius:BorderRadius.circular(11),
+                              borderSide:BorderSide(
+                              color: Colors.blueAccent,width:2) 
+                              ),
+                                                          
                               labelText: 'Password',
-                              suffixIcon: IconButton(
-                                  icon: Icon(Icons.remove_red_eye,
-                                      color: Colors.red),
-                                  onPressed: () {
-                                    //Add you Logic
-                                  }))),
-                      Container(height: 11),
-                      ElevatedButton(
-                        child: Text(
-                          "Login",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        onPressed: () {
-                          //Add ypu Logic
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.blue,
-                        ),
-                      )
-                    ]))));
-  }
-}
+                              suffixIcon:IconButton(
+                                icon:Icon(Icons.remove_red_eye,color:Colors.red),
+                                onPressed:(){
+                                  //Add you Logic                                  
+                                }
+                               )
+                              ) 
+                             ),
+                               Container(height:11),
+                               ElevatedButton(
+                               child:Text("Login",style:TextStyle(color:Colors.white),),
+                                  onPressed:()
+                                   {
+                                     String uEmail = emailText.text.toString();
+                                     String uPass= passText.text.toString();
+                                     print("Email:$uEmail, Pass:$uPass");    //Add ypu Logic
+                                    }, style: ElevatedButton.styleFrom(
+                                       primary: Colors.blue,),)
+                                   ]
+                          
+                          )        
+                         )
+                        )
+                       );
+                      }
+                     }
